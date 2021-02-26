4f83d320808f26c67859564e79e7592e69a683a8
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-26 09:27:54 -0800

    write and fail first test to get cost of bread without the deal

--- Bakery.Tests/ModelTests/BreadPastryTests.cs
+++ Bakery.Tests/ModelTests/BreadPastryTests.cs
@@ -1,10 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Bakery.Models;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class BreadTests
+  {
+    [TestMethod]
+    public void OrderCost_GetCostOfBread_Int()
+    {
+      Bread newOrder = new Bread(5);
+      int result = newOrder.Cost();
+      Assert.AreEqual(25, result);
+    }
+  }
+
+
+
+  [TestClass]
+  public class PastryTests
   {
     // Test methods go here
   }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,6 +5,17 @@ namespace Bakery.Models
 {
   public class Bread
   {
+    public int OrderNum {get; set;}
+    public int OrderCost {get; set;}
 
+    public Bread(int order)
+    {
+
+    }
+
+    public int Cost()
+    {
+      return 0;
+    }
   }
 }
