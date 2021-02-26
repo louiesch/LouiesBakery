21e16504202236975d7ba76f7f4a37fc6a66489a
Louie Schoenknecht <luisesch97@gmail.com>
2021-02-26 15:15:37 -0800

    pass test for two loaves of bread price

--- Bakery.Tests/ModelTests/BreadPastryTests.cs
+++ Bakery.Tests/ModelTests/BreadPastryTests.cs
@@ -10,7 +10,7 @@ namespace Bakery.Tests
     [TestMethod]
     public void Price_GetPriceOfBread_Int()
     {
-      Assert.AreEqual(5, Bread.Price(1));
+      Assert.AreEqual(10, Bread.Price(2));
     }
 
     // [TestMethod]
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -3,12 +3,18 @@ namespace Bakery.Models
   public class Bread
   {
     public int NumBread {get; set;}
+    
+    //Bread: Buy 2, get 1 free. A single loaf costs $5.
     public static int Price(int num)
     {
       int NumBread = num;
       int price = 5;
 
       if (NumBread == 1)
+      {
+        return num * price;
+      }
+      else if (NumBread == 2)
       {
         return num * price;
       }
